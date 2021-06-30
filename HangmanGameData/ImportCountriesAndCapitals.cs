af29f5cc740f49dcc0c6937cb2557a9f3baa70b7
Ernest Piekarczyk <aboxte@gmail.com>
2021-06-30 19:50:36 +0200

    Create import Countries function

---
+++ HangmanGameData/ImportCountriesAndCapitals.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using HangmanGameModels;
+
+namespace HangmanGameData
+{
+    static class ImportCountriesAndCapitals
+    {
+        private static Country[] GetCountries()
+        {
+            Country[] countries = { };
+
+            var FileStream = new FileStream(path: ConfigurationManager.AppSettings["CountriesAndCapitalsPath"], FileMode.Open, FileAccess.Read);
+            var StreamReader = new StreamReader(FileStream, Encoding.UTF8);
+            string line;
+
+            while ((line = StreamReader.ReadLine()) != null)
+            {
+                string[] splitLine;
+                splitLine = line.Split("|");
+
+                countries.Append(new Country { Name = splitLine[0], CapitalName = splitLine[1] });
+            }
+
+            return countries;
+        }    
+
+	}
+}
